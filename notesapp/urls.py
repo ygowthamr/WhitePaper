7370a830d07f01fef3621414c9b60817623ece11
CHARU AWASTHI <148922861+Charu19awasthi@users.noreply.github.com>
2025-01-19 18:23:02 +0530

    Update urls.py

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('',views.index,name="mynotepad"),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    # Add other routes for your project
 ]
